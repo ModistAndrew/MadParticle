e6680f79703d19f24b61e039d762af2e9dc53984
ModistAndrew <zhujinxuan2005@163.com>
2025-06-16 21:42:52 +0800

    remove ref surfacer; headless mode

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 mod aabb;
 mod generator;
 mod grid;
-mod ref_surfacer;
 mod simulator;
 mod surfacer;
 mod wrapper;
@@ -10,6 +9,7 @@ use crate::aabb::Aabb;
 use crate::generator::Generator;
 use crate::wrapper::{CommonParams, SimulatorParams, SurfacerParams, Wrapper};
 use bevy::prelude::*;
+use std::env::args;
 use std::io::Write;
 
 #[derive(Component)]
@@ -20,11 +20,20 @@ struct WrapperComponent(Wrapper);
 struct SurfaceMeshHandle(Handle<Mesh>);
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
-        .add_systems(Update, pbd_step)
-        .run();
+    let args: Vec<String> = args().collect();
+    let headless = args.len() > 1 && args[1] == "--headless";
+    if headless {
+        let mut wrapper = init_wrapper();
+        loop {
+            wrapper.step();
+        }
+    } else {
+        App::new()
+            .add_plugins(DefaultPlugins)
+            .add_systems(Startup, setup)
+            .add_systems(Update, pbd_step)
+            .run();
+    }
 }
 
 fn init_wrapper() -> Wrapper {
@@ -75,7 +84,8 @@ fn setup(
 
     let wrapper = init_wrapper();
     let radius = wrapper.radius;
-    let sphere_mesh = meshes.add(Mesh::from(Sphere::new(radius)));
+    let particle_mesh = meshes.add(Mesh::from(Sphere::new(radius)));
+    let boundary_mesh = meshes.add(Mesh::from(Sphere::new(radius / 5.0)));
     let particle_material = materials.add(StandardMaterial {
         base_color: Color::srgb(0.8, 0.2, 0.2),
         ..default()
@@ -91,24 +101,23 @@ fn setup(
         .enumerate()
         .for_each(|(i, &p)| {
             commands.spawn((
-                Mesh3d(sphere_mesh.clone()),
+                Mesh3d(particle_mesh.clone()),
                 MeshMaterial3d(particle_material.clone()),
                 Transform::from_translation(p),
                 Index(i),
             ));
         });
-
-    // wrapper
-    //     .simulator
-    //     .boundary_positions()
-    //     .iter()
-    //     .for_each(|&p| {
-    //         commands.spawn((
-    //             Mesh3d(sphere_mesh.clone()),
-    //             MeshMaterial3d(boundary_material.clone()),
-    //             Transform::from_translation(p),
-    //         ));
-    //     });
+    wrapper
+        .simulator
+        .boundary_positions()
+        .iter()
+        .for_each(|&p| {
+            commands.spawn((
+                Mesh3d(boundary_mesh.clone()),
+                MeshMaterial3d(boundary_material.clone()),
+                Transform::from_translation(p),
+            ));
+        });
     commands.spawn(WrapperComponent(wrapper));
 }
 
